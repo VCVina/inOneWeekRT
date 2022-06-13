e0281ac5480b58cc7b6c4f32effe9a2f31043b3c
VCVina <28703833+VCVina@users.noreply.github.com>
2022-06-13 13:57:18 +0900

    Change color and spp

--- inOneWeekRT/main.cpp
+++ inOneWeekRT/main.cpp
@@ -68,20 +68,20 @@ int main() {
     outImage.open("image.ppm");
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);//w除以wh比
-    const int samples_per_pixel = 30;//spp次数
+    const int samples_per_pixel = 50;//spp次数
     const int max_depth = 50;//最大最大的递归次数
     
     // World
     hittable_list world;
     
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_ground = make_shared<lambertian>(color(0.3, 0.8, 0.5));
     auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
     auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
     auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.5,    0.0, -2.0),   0.5, material_left));
     world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Camera
